f3f7eadde4e523bcadab6cf46fa6c69c0ebb711b
Omri Hadadi <77164586+omrihadadi@users.noreply.github.com>
2023-10-02 18:53:28 +0300

    Update#3 genai_camp_assistant.py

--- Paradox_Assignment/genai_camp_assistant.py
+++ Paradox_Assignment/genai_camp_assistant.py
@@ -1,10 +1,11 @@
 import openai
+import re
 
 min_age = None
 max_age = None
 
 # Initialize the OpenAI API
-openai.api_key = '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'
+openai.api_key = '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'
 
 # Fetching Camp Details using Chat Model
 
@@ -12,12 +13,21 @@ def get_camp_details():
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": "Describe a fictional GenAI Summer Camp, detailing its offerings, values, policies, location, dates, pricing, and age range."}
+            {"role": "user", "content": "Describe a fictional GenAI Summer Camp, detailing its offerings, values, policies, location, dates, pricing, min age and max age in numbers."}
         ]
     )
     return response.choices[0].message['content']
 
 camp_details = get_camp_details()
+
+def extract_age_from_response(response_text):
+    """Extract age from the OpenAI's response text."""
+    age_matches = re.findall(r'(\d+)', response_text)
+    if age_matches:
+        return int(age_matches[0])
+    else:
+        raise ValueError("Couldn't extract age from the response.")
+
 
 def initialize_age_range():
     global min_age, max_age
@@ -26,19 +36,25 @@ def initialize_age_range():
     response_min_age = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": "What's the minimum age for the GenAI Summer Camp?"}
+            {"role": "user", "content": "What is the youngest age allowed for the GenAI Summer Camp? Please answer with just a number."}
         ]
     )
-    min_age = int(response_min_age['choices'][0]['message']['content'])
+    temp_min_age = extract_age_from_response(response_min_age['choices'][0]['message']['content'])
 
     # Ask OpenAI for the maximum age
     response_max_age = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": "What's the maximum age for the GenAI Summer Camp?"}
+            {"role": "user", "content": "What is the oldest age allowed for the GenAI Summer Camp? Please answer with just a number."}
         ]
     )
-    max_age = int(response_max_age['choices'][0]['message']['content'])
+    temp_max_age = extract_age_from_response(response_max_age['choices'][0]['message']['content'])
+
+    # Ensure the age range is logical
+    min_age = min(temp_min_age, temp_max_age)
+    max_age = max(temp_min_age, temp_max_age)
+
+
 
 # Router Prompt
 
@@ -86,23 +102,24 @@ def application_prompt():
         print("Application successfully received! We'll be in touch.")
     else:
         print(f"Sorry, this camp is for kids aged between {min_age} and {max_age}.")
+        parent_data = {}  # This ensures parent_data is always defined
 
-    return parent_data
+    
 
 # Main Function
 
 def main():
     print("Hello! Welcome to GenAI Summer Camp's assistant. Do you want to ask a question about the camp or sign your kid up?")
-    parent_input = input()
-
-    route = router_prompt(parent_input)
-    if route == "question":
-        parent_question = input("Please ask your question: ")
-        print(answer_question(parent_question))
-    elif route == "sign_up":
-        print(application_prompt())
-    else:
-        print("Sorry, I didn't understand that. Please type your intent clearly.")
+    while True: 
+        parent_input = input()
+        route = router_prompt(parent_input)
+        if route == "question":
+          parent_question = parent_input
+          print(answer_question(parent_question))
+        elif route == "sign_up":
+          print(application_prompt())
+        else:
+          print("Sorry, I didn't understand that. Please type your intent clearly.")
 
 if __name__ == "__main__":
     main()
